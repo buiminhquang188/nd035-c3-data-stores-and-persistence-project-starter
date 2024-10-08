42bbfa64564d66500ceed65466a921ed8f346821
Quang Bui <buiminhquang188@gmail.com>
2024-10-08 18:38:50 +0700

    Update: Initialize Data & Postman Collection

--- starter/critter/src/main/resources/data.sql
+++ starter/critter/src/main/resources/data.sql
@@ -27,4 +27,51 @@ VALUES (1, 'MONDAY'),
        (4, 'THURSDAY'),
        (5, 'FRIDAY'),
        (6, 'SATURDAY'),
-       (7, 'SUNDAY')
+       (7, 'SUNDAY');
+
+INSERT INTO user (name, phone_number, notes, role_id)
+VALUES ('Clark', '09091232211', 'Hello Clark', 1);
+INSERT INTO user (name, phone_number, notes, role_id)
+VALUES ('Bruce', '99221122331', 'Hello Bruce', 2);
+INSERT INTO user (name, phone_number, notes, role_id)
+VALUES ('Walker', null, null, 2);
+
+
+INSERT INTO pet (id, name, age, birth_date, gender, notes, pet_type_id, user_id)
+VALUES (1, 'Powder', 1, '2020-10-08', 'MALE', 'Hello Powder', 2, 1),
+       (2, 'Bell', 1, '2023-10-01', 'FEMALE', 'Hello Bell', 1, 1);
+
+INSERT INTO schedule (id, date)
+VALUES (1, '2019-12-21 17:00:00'),
+       (2, '2019-12-16 17:00:00');
+
+INSERT INTO schedule_employee (schedule_id, user_id)
+VALUES (1, 2),
+       (2, 2),
+       (1, 3),
+       (2, 3);
+
+INSERT INTO schedule_pet (pet_id, schedule_id)
+VALUES (1, 1),
+       (2, 1),
+       (1, 2),
+       (2, 2);
+
+INSERT INTO schedule_skill (schedule_id, skill_id)
+VALUES (2, 1),
+       (2, 3),
+       (1, 5);
+
+INSERT INTO user_operation_time (user_id, operation_time_id)
+VALUES (2, 1),
+       (2, 2),
+       (2, 7),
+       (3, 1),
+       (3, 2),
+       (3, 7);
+
+INSERT INTO user_skill (user_id, skill_id)
+VALUES (3, 3),
+       (3, 1),
+       (2, 2),
+       (2, 1);
